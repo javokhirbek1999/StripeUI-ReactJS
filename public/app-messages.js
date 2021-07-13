8259dc26b2181599175061639c547cd09ea4d07b
Javokhirbek <54243963+javokhirbek1999@users.noreply.github.com>
2021-07-13 14:06:13 +0500

    Update app-messages.js

--- public/app-messages.js
+++ public/app-messages.js
@@ -1,1 +1,11 @@
+import React from 'react'
+import {useGlobalContext} from '../src/context'
 // App messages
+
+const {text,sender} = useGlobalContext();
+
+const Messages = () => {
+  
+}
+
+export default Messages;
